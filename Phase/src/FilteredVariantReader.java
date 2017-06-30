05599f75c7a16b63dbc3efcc89f6bbafa3887d16
Paul Hager <paul@hager94.net>
2017-06-30 11:29:06 +0200

    test

--- Phase/src/FilteredVariantReader.java
+++ Phase/src/FilteredVariantReader.java
@@ -1,7 +1,5 @@
-import java.io.BufferedReader;
 import java.io.EOFException;
 import java.io.File;
-import java.io.FileReader;
 import java.io.IOException;
 import java.io.RandomAccessFile;
 import java.util.ArrayList;
@@ -17,7 +15,6 @@ import htsjdk.variant.variantcontext.VariantContextBuilder;
 public class FilteredVariantReader {
 
 	private RandomAccessFile raFile;
-	private BufferedReader fileReader;
 	private String fileName;
 	private boolean vcf = false;
 	private HashMap<String, Long> contigPointers;
@@ -29,16 +26,6 @@ public class FilteredVariantReader {
 		try {
 			raFile = new RandomAccessFile(inFile, "r");
 			contigPointers = new HashMap<String, Long>();
-			
-			/*
-<<<<<<< HEAD
-			raFile = new RandomAccessFile(inFile, "r");
-			contigPointers = new HashMap<String, Long>();
-=======
-			fileName = inFile.getName();
-			fileReader = new BufferedReader(new FileReader(inFile));
->>>>>>> branch 'master' of https://github.com/paulhager/smart-phase.git
-			*/
 
 			String line;
 
