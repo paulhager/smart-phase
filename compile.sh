de3c9409079c4828d7d760b0f049688a6abe568b
timjeske <tim.jeske@yahoo.de>
2019-06-07 10:50:00 +0200

    fix tar to jar xf

--- compile.sh
+++ compile.sh
@@ -4,8 +4,8 @@ mkdir ./compile
 javac -cp ./Phase/src/src/smartPhase ./Phase/src/src/smartPhase/*.java -d ./compile/ -classpath ./Phase/libraries/picard.jar:./Phase/libraries/commons-cli-1.4.jar 
 cp ./Phase/libraries/*.jar ./compile/
 cd compile/
-tar xf picard.jar
-tar xf commons-cli-1.4.jar
+jar xf picard.jar
+jar xf commons-cli-1.4.jar
 rm picard.jar
 rm commons-cli-1.4.jar
 jar -cvfm smartPhase.jar ../Phase/META-INF/MANIFEST.MF .
